4b0c3a4d66c80d5e3af6d772cebf43865b0ba120
Taras-Lozovoy <taras.lozovoy.work@gmail.com>
2022-05-19 14:08:33 +0300

    my commit

--- src/Components/Common/AnimatedPage/AnimatedPage.jsx
+++ src/Components/Common/AnimatedPage/AnimatedPage.jsx
@@ -3,26 +3,25 @@ import { motion } from 'framer-motion'
 import classes from './AnimatedPage.module.css'
 
 const AnimatedPage = (props) => {
-    const { children } = props
+  const {children, direction} = props
+  const animations = {
+    initial: {opacity: 0, x: direction === 'right' ? -1000 : 1000},
+    animate: {opacity: 1, x: 0},
+    exit: {opacity: 0, x: direction === 'right' ? 1000 : -1000},
+  }
 
-    const animations = {
-        initial: { opacity: 0, x: 1000 },
-        animate: { opacity: 1, x: 0 },
-        exit: { opacity: 0, x: -1000 }
-    }
-
-    return (
-        <motion.div
-            variants={animations}
-            initial="initial"
-            exit="exit"
-            animate="animate"
-            transition={{ type: "keyframes", duration: 1 }}
-            className={classes.main}
-        >
-            {children}
-        </motion.div>
-    )
+  return (
+      <motion.div
+        variants={animations}
+        initial="initial"
+        exit="exit"
+        animate="animate"
+        transition={{duration: 1}}
+        className={classes.main}
+      >
+        {children}
+      </motion.div>
+  )
 }
 
 export default AnimatedPage
