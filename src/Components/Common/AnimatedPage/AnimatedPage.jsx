9ca14c9407fd3d3cd89501235903635af4436be0
Taras-Lozovoy <taras.lozovoy.work@gmail.com>
2022-05-19 15:17:46 +0300

    fix component view

--- src/Components/Common/AnimatedPage/AnimatedPage.jsx
+++ src/Components/Common/AnimatedPage/AnimatedPage.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import classes from './AnimatedPage.module.css'
-
-const AnimatedPage = (props) => {
-  const {children, direction} = props
-  const animations = {
-    initial: {opacity: 0, x: direction === 'right' ? -1000 : 1000},
-    animate: {opacity: 1, x: 0},
-    exit: {opacity: 0, x: direction === 'right' ? 1000 : -1000},
-  }
-
-  return (
-      <motion.div
-        variants={animations}
-        initial="initial"
-        exit="exit"
-        animate="animate"
-        transition={{duration: 1}}
-        className={classes.main}
-      >
-        {children}
-      </motion.div>
-  )
-}
-
-export default AnimatedPage
+// import React from 'react'
+// import {AnimatePresence, motion} from 'framer-motion'
+// import classes from './AnimatedPage.module.css'
+//
+// const AnimatedPage = (props) => {
+//     const {children, direction} = props
+//     const animations = {
+//         initial: {opacity: 0, x: direction === 'right' ? -1000 : 1000},
+//         animate: {opacity: 1, x: 0},
+//         exit: {opacity: 0, x: direction === 'right' ? 1000 : -1000},
+//     }
+//
+//     return (
+//         <AnimatePresence>
+//             <motion.div
+//                 variants={animations}
+//                 initial="initial"
+//                 exit="exit"
+//                 animate="animate"
+//                 transition={{duration: 1}}
+//                 className={classes.main}
+//             >
+//                 {children}
+//             </motion.div>
+//         </AnimatePresence>
+//     )
+// }
+//
+// export default AnimatedPage
